0061cb03a483c21de2c84537d5487a4be474d638
Grant Spilsbury <grantspilsbury@gmail.com>
2018-01-07 08:03:16 +1100

    Added extra emotions

--- App.js
+++ App.js
@@ -15,21 +15,34 @@ export default class App extends Component<{}> {
   constructor(props) {
     super(props);
     this.handleButtonPress = this.handleButtonPress.bind(this);
-    this.onPressHappyButton = this.onPressHappyButton.bind(this);
-    this.onPressHappySadButton = this.onPressHappySadButton.bind(this);
-    this.onPressSadButton = this.onPressSadButton.bind(this);
+    this.onPressHappyButton3 = this.onPressHappyButton3.bind(this);
+    this.onPressHappyButton2 = this.onPressHappyButton2.bind(this);
+    this.onPressHappyButton1 = this.onPressHappyButton1.bind(this);
+
+    this.onPressSadButton1 = this.onPressSadButton1.bind(this);
+    this.onPressSadButton2 = this.onPressSadButton2.bind(this);
+    this.onPressSadButton3 = this.onPressSadButton3.bind(this);
+
   }
 
-  onPressHappyButton() {
-    this.handleButtonPress(2);
-  }
-
-  onPressHappySadButton() {
+  onPressHappyButton1() {
     this.handleButtonPress(1);
   }
+  onPressHappyButton2() {
+    this.handleButtonPress(2);
+  }
+  onPressHappyButton3() {
+    this.handleButtonPress(3);
+  }
 
-  onPressSadButton() {
-    this.handleButtonPress(0);
+  onPressSadButton1() {
+    this.handleButtonPress(-1);
+  }
+  onPressSadButton2() {
+    this.handleButtonPress(-2);
+  }
+  onPressSadButton3() {
+    this.handleButtonPress(-3);
   }
 
   // onEmergencyPress() {
@@ -68,7 +81,7 @@ export default class App extends Component<{}> {
           <TouchableHighlight
             underlayColor="rgba(232,244,248,0.5)"
             style={styles.button}
-            onPress={this.onPressHappyButton}>
+            onPress={this.onPressHappyButton3}>
             <Image
               style={styles.image}
               source={require('./images/happy2.png')}
@@ -77,19 +90,50 @@ export default class App extends Component<{}> {
           <TouchableHighlight
             underlayColor="rgba(232,244,248,0.5)"
             style={styles.button}
-            onPress={this.onPressHappySadButton}>
+            onPress={this.onPressSadButton1}>
             <Image
               style={styles.image}
-              source={require('./images/happysad2.png')}
+              source={require('./images/sad1.png')}
+            />
+          </TouchableHighlight>
+        </View>
+        <View style={styles.rowContainer}>
+          <TouchableHighlight
+            underlayColor="rgba(232,244,248,0.5)"
+            style={styles.button}
+            onPress={this.onPressHappyButton2}>
+            <Image
+              style={styles.image}
+              source={require('./images/happy1.png')}
             />
           </TouchableHighlight>
           <TouchableHighlight
             underlayColor="rgba(232,244,248,0.5)"
             style={styles.button}
-            onPress={this.onPressSadButton}>
+            onPress={this.onPressSadButton2}>
             <Image
               style={styles.image}
               source={require('./images/sad2.png')}
+            />
+          </TouchableHighlight>
+        </View>
+        <View style={styles.rowContainer}>
+          <TouchableHighlight
+            underlayColor="rgba(232,244,248,0.5)"
+            style={styles.button}
+            onPress={this.onPressHappyButton1}>
+            <Image
+              style={styles.image}
+              source={require('./images/happy3.png')}
+            />
+          </TouchableHighlight>
+          <TouchableHighlight
+            underlayColor="rgba(232,244,248,0.5)"
+            style={styles.button}
+            onPress={this.onPressSadButton3}>
+            <Image
+              style={styles.image}
+              source={require('./images/sad3.png')}
             />
           </TouchableHighlight>
         </View>
@@ -112,19 +156,17 @@ const styles = StyleSheet.create({
     color: '#fff'
   },
   rowContainer: {
-    justifyContent: 'space-between',
-    flexDirection: 'column',
-    marginBottom: 10
+    flexDirection: 'row',
+    justifyContent: 'space-between'
   },
   button: {
     alignItems: 'center',
     padding: 10,
-    borderRadius: 60
+    borderRadius: 55
   },
   image: {
-    width: 120,
-    height: 120,
-    paddingBottom: 20,
+    width: 110,
+    height: 110,
     resizeMode: Image.resizeMode.contain,
   }
 });
